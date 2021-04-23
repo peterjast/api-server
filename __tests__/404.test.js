e28cd6cc91787b7fe2c78699263e1a8793e5f7f1
Peter Staker <peterjast@gmail.com>
2021-04-23 16:58:01 -0700

    updating tests

--- __tests__/404.test.js
+++ __tests__/404.test.js
@@ -7,15 +7,15 @@ const mockRequest = supertest(server);
 describe('404:', () => {
 
   it('should respond with a 404 on bad route', async() => {
-    return mockRequest.get('/bad-route').then(data => {
-      expect(data.status).toBe(404);
-    })
+    const data = await mockRequest.get('/bad-route');
+    console.log(data);
+    expect(data.status).toBe(404);
   });
 
   it('should respond with a 404 on bad method', async() => {
-    return mockRequest.post('/clothes/1').then(data => {
-      expect(data.status).toBe(404);
-    })
+    const data = await mockRequest.post('/clothes/1');
+    console.log(data);
+    expect(data.status).toBe(404);
   });
 
 })
--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -7,7 +7,7 @@ const mockRequest = supertest(server);
 describe('***LOGGER***', () => {
 
   it('should log request method and path on GET: /clothes', async() => {
-    const consoleSpy = jest.spyOn(console, 'log');
+    const consoleSpy = await jest.spyOn(console, 'log');
     const response = await mockRequest.get('/clothes');
     expect(response.status).toBe(200); //status code
     expect(consoleSpy).toHaveBeenCalledWith('Request data:', 'GET', '/clothes');
