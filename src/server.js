dd9d8519eebbd6b7b2816faa019a74d1e1da2104
Peter Staker <peterjast@gmail.com>
2021-04-26 21:33:52 -0700

    changes to configure atlas and mongodb uri

--- src/server.js
+++ src/server.js
@@ -3,7 +3,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://localhost:27017/data-collection';
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true }; 
 mongoose.connect(MONGODB_URI, options);
