0df370ce85891a8ced9ca6e29ed78f456123c2ee
Peter Staker <peterjast@gmail.com>
2021-06-09 14:13:08 -0700

    removed tests

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -6,12 +6,12 @@ const mockRequest = supertest(server);
 
 describe('***LOGGER***', () => {
 
-  it('should log request method and path on GET: /clothes', async() => {
+  it('should log request method and path on GET: /product', async() => {
     const consoleSpy = await jest.spyOn(console, 'log');
-    mockRequest.get('/clothes')
+    mockRequest.get('/product')
     .then(response => {
       expect(response.status).toBe(200); //status code
-      expect(consoleSpy).toHaveBeenCalledWith('Request data:', 'GET', '/clothes');
+      expect(consoleSpy).toHaveBeenCalledWith('Request data:', 'GET', '/product');
     })
     .catch(err => console.log(err.message));
   });
